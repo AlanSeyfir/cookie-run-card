2f0745e647e94624283359c044a06632bb7057e0
AlanSeyfir <seyfirdeveloper@gmail.com>
2025-02-11 22:19:38 -0600

    Initial project with TS and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="container p-4 text-5xl bg-sky-900 text-sky-50 font-bold text-center">Cookie Run 🍪</h1>
+
+      <header>
+        Info of the player
+      </header>
+
+      <section>
+        Characters (Cookies)
+      </section>
+
+      <main>
+        <section>
+          Card
+        </section>
+      </main>
+
+      <section>
+        Card options
+      </section>
+
+      <footer>
+        Socials
+      </footer>
+    </>
+  )
+}
+
+export default App
